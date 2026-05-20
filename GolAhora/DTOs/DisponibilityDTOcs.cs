90eb3c0496819e2a8c80749abea8b1b27c98671a
Cristian <cristianseg0122@gmail.com>
2026-05-20 16:49:50 -0300

    Agrego DTOS de user

--- GolAhora/DTOs/DisponibilityDTOcs.cs
+++ GolAhora/DTOs/DisponibilityDTOcs.cs
@@ -1,9 +1,13 @@
-﻿namespace GolAhora.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GolAhora.DTOs
 {
     public class DisponibilityDTO
     {
         public DayOfWeek day { get; set; }
+        [Range(typeof(TimeSpan), "00:00:00", "23:59:59", ErrorMessage = "El valor no puede exceder las 24 horas o respte este formato: hh:mm.")]
         public TimeSpan startTime { get; set; }
+        [Range(typeof(TimeSpan), "00:00:00", "23:59:59", ErrorMessage = "El valor no puede exceder las 24 horas o respte este formato: hh:mm.")]
         public TimeSpan endTime { get; set; }
         public bool isAvailable { get; set; }
         public int courtId { get; set; }
---
+++ GolAhora/DTOs/UserDto.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Identity;
+using System.Diagnostics.Contracts;
+
+namespace GolAhora.DTOs
+{
+    public class UserDto
+    {
+        public string name { get; set; } = null!;
+        public string lastName { get; set; } = null!;
+        public string DNI { get; set; } = null!; 
+        public string userName { get; set; } = null!;
+        public string password { get; set; } = null!;
+        public string phoneNumber { get; set; } = null!;
+        public bool isActive { get; set; }
+    }
+
+    public class ClientDto: UserDto
+    {
+        public int numberPartner { get; set; }
+        public int idTeam { get; set; }
+    }
+
+    public class PersonalClubDto: UserDto
+    {
+            public string legajo { get; set; } = null!;
+            public DateTime startDate { get; set; }
+            public string turno { get; set; } = null!;
+    }
+
+    public class AdminDto: PersonalClubDto
+    {
+            public int accessLevel { get; set; }
+    }
+
+    public class EmployeeDto: PersonalClubDto
+    {
+        public string sector { get; set; } = null!;
+    }
+
+    public class ProfessorDto: PersonalClubDto 
+    {
+        public string specialty { get; set; } = null!;
+    }
+}
