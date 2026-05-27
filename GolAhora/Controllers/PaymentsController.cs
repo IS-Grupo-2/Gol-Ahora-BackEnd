de50c6702b932c35714707699d88fb5aa5bfbf5c
niasua <nsuarez2410@gmail.com>
2026-05-27 18:34:56 -0300

    Se implementa rf48

---
+++ GolAhora/Controllers/PaymentsController.cs
@@ -0,0 +1,45 @@
+﻿using GolAhora.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GolAhora.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaymentsController : Controller
+    {
+        private readonly Data.AppContext _context;
+
+        public PaymentsController(Data.AppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/payments --> RF48
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Payments>>> GetPayments()
+        {
+            return await _context.Payments
+                .Include(p => p.client)
+                .Include(p => p.discount)
+                .ToListAsync();
+        }
+
+        //GET: api/payments/id --> RF48
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Payments>> GetPayment(int id)
+        {
+            var payment = await _context.Payments
+                .Include(p => p.client)
+                .Include(p => p.discount)
+                .FirstOrDefaultAsync(p => p.idPayment == id);
+
+            if (payment == null)
+            {
+                return NotFound(new { message = $"Pago con ID {id} no encontrado." });
+            }
+
+            return payment;
+        }
+    }
+}
